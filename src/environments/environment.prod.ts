a82356766f77a33bdbdf18fa33dadc272c22e60f
Shaik Mohammad Muzmil <48669082+muzmil531@users.noreply.github.com>
2024-07-05 18:46:37 +0530

    Deployed Version

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,38 @@
 // src/environments/environment.prod.ts
 export const environment = {
     production: true,
-    apiUrl: 'https://api.example.com'
+    firebaseConfig: {
+      apiKey: "AIzaSyBz7rI9K7VGMmO1GbRX7pstXOZgv5-z__E",
+      authDomain: "ncmedianewsportal-v2.firebaseapp.com",
+      projectId: "ncmedianewsportal-v2",
+      storageBucket: "ncmedianewsportal-v2.appspot.com",
+      messagingSenderId: "1090074501186",
+      appId: "1:1090074501186:web:2a7fc30151dc7530be0ddb",
+      measurementId: "G-9F8X1GRERE"
+    },
+  
+    base_url: 'https://us-central1-neticharithra-ncmedia.cloudfunctions.net/api/',
+    // base_url: 'https://nc-media-management-app-be-neti-charithras-projects.vercel.app/',
+    // base_url: 'https://viridian-slug-sari.cyclic.app/',
+    base_url_upload_files: 'https://nc-media-managament-app-be-files-upload.onrender.com/',
+    // base_url: 'http://192.168.10.185:3000/',
+    // base_url_upload_files: 'http://localhost:3001/',
+    googleDrive: {
+      // insert credentials here
+      apiKey: "AIzaSyDXivxGi5FHfWDAW1U1qcsjlH0_XxBur7Y",
+      clientId: "1049760491527-hpmiohl8nfa361qgmp79u1a5flcqcmrp.apps.googleusercontent.com"
+      //
+    },
+    // replace folders object here
+    folders: {
+      create: true,
+      moveFiles: false
+    },
+    //
+    upload: {
+      simple: true,
+      multipart: false,
+      resumable: false,
+    }
   };
   
